9ffb74b42cb50d79ea28c37945075b52a50fcf66
zahhar <zahhar@gmail.com>
2024-08-26 10:27:54 +0200

    Path adjustments to run in VPS

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get update && apt-get upgrade -y
+#apt-get update && apt-get upgrade -y #takes too long and may still ask for user interactions
 
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 az login --service-principal \
@@ -7,7 +7,7 @@ az login --service-principal \
          --password ${AZ_PWD} \
          --tenant 5537b611-23f3-428b-86e7-808c0da2ed8c
 
-az deployment sub create --location westeurope --template-file az_rg.bicep --parameters admPwd=${ADM_PWD} masterIp=${MASTER_IP}
+az deployment sub create --location westeurope --template-file ./az_rg.bicep --parameters admPwd=${ADM_PWD} masterIp=${MASTER_IP}
 
 curl -sfL https://get.k3s.io | sh -
 sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
@@ -19,4 +19,4 @@ chmod 700 get_helm.sh
 kubectl create namespace nextcloud
 helm repo add nextcloud https://nextcloud.github.io/helm/
 helm repo update
-helm install nextcloud nextcloud/nextcloud -n nextcloud -f ./nc/nc_values.yaml --set externalDatabase.password=${ADM_PWD} --set nextcloid.password=${ADM_PWD}
+helm install nextcloud nextcloud/nextcloud -n nextcloud -f ./nc_values.yaml --set externalDatabase.password=${ADM_PWD} --set nextcloid.password=${ADM_PWD}
